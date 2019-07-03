cad00f4f87a3d5315e1c47c82d274ca2b8fdfbb1
aniketp319 <aniket.patil1@somaiya.edu>
2019-07-03 20:27:29 +0530

    Main, Top activity updated

--- app/src/main/java/com/example/outfitplanner/MainActivity.java
+++ app/src/main/java/com/example/outfitplanner/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.outfitplanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToTop(View view){
+        Intent intent = new Intent(this, TopActivity.class);
+        startActivity(intent);
+    }
+
+    public void exitMain(View view){
+        finish();
+        System.exit(0);
     }
 }
--- app/src/main/java/com/example/outfitplanner/TopActivity.java
+++ app/src/main/java/com/example/outfitplanner/TopActivity.java
@@ -1,7 +1,9 @@
 package com.example.outfitplanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TopActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class TopActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top);
+    }
+
+    public void chooseTop(View view){
+        //start camera module
+
+        //save the image
+
+        //set the image in imageView
+    }
+
+    public void goToBottom(View view){
+        Intent intent = new Intent(this, BottomActivity.class);
+        startActivity(intent);
+    }
+
+    public void goBack(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
