d24a12a9aa84ee2082edc47a99d3c3ae28429078
aniketp319 <aniket.patil1@somaiya.edu>
2019-07-03 15:54:25 +0530

    activities added

---
+++ app/src/main/java/com/example/outfitplanner/BottomActivity.java
@@ -0,0 +1,13 @@
+package com.example.outfitplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BottomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom);
+    }
+}
