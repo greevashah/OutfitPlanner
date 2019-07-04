fa86878b9462dd37f454949376021b4899b128a8
raj-vora <raj.jv@somaiya.edu>
2019-07-04 10:46:47 +0530

    Updated top, bottom, main, added app icon

--- app/src/main/java/com/example/outfitplanner/BottomActivity.java
+++ app/src/main/java/com/example/outfitplanner/BottomActivity.java
@@ -1,7 +1,9 @@
 package com.example.outfitplanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class BottomActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class BottomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom);
+    }
+
+    public void chooseBottom(View view){
+        //start camera module
+
+        //save the image
+
+        //set the image in imageView
+    }
+
+    public void goToTop(View view){
+        Intent intent = new Intent(this, BottomActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToResult(View view){
+        Intent intent = new Intent(this, ResultActivity.class);
+        startActivity(intent);
     }
 }
