a75bf74ddaf2dc84f88d673596d337ca895276f7
raj-vora <raj.jv@somaiya.edu>
2019-07-28 20:44:44 +0530

    tried to make color dictionary

---
+++ Color/colordictionary.java
@@ -0,0 +1,11 @@
+class colordictionary {
+  public static void main(String[] args) {
+    String[] neutral = {"antiquewhite", "beige", "floralwhite", "ghostwhite", "navajowhite", "whitesmoke", "white", "ivory", "mintcream", "lightgrey", "lightgray", "sandybrown", "tan", "aqua", "aquamarine", "darkgray", "darkgrey", "darkslategray", "darkslategrey", "dimgray", "dimgrey", "gray", "grey", "lightslategray", "lightslategrey", "slategray", "slategray", "darkkhaki", "khaki", "olivedrab", "olive", "navy", "brown", "rosybrown", "saddlebrown", "sandybrown", "black"};
+     
+     String[] primary = {"darkred", "indianred", "mediumvioletred", "palevioletred", "red", "lightgoldenrodyellow", "lightyellow", "yellow", "aliceblue", "blue", "cadetblue", "cornflowerblue", "darkblue", "darkslateblue", "deepskyblue", "dodgerblue", "lightblue", "lightskyblue", "lightsteelblue", "mediumblue", "mediumslateblue", "midnightblue", "powderblue", "royalblue", "skyblue", "slateblue", "steelblue"};
+
+     String[] secondary = {"darkorange", "orange", "darkviolet", "violet", "darkgreen", "darkolivegreen", "darkseagreen", "forestgreen", "green", "lawngreen", "lightgreen", "lightseagreen", "lime", "limegreen", "mediumseagreen", "mediumspringgreen", "palegreen", "seagreen", "springgreen"};
+
+     String[] tertiary = {"greenyellow", "yellowgreen", "orangered", "blueviolet"};
+  }
+}
